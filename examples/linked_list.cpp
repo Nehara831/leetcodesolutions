bf9d378e67bdeeabfc8f4b4103660cea1ea3af9c
Nehara831 <tnehara831@gmail.com>
2022-09-21 10:50:30 +0530

    minor changes

--- examples/linked_list.cpp
+++ examples/linked_list.cpp
@@ -15,12 +15,29 @@ class Node{
 };
 void printNode(Node* n){
     int count=0;
-        while(n!=NULL){
-            cout<<n->data<<endl;
-            n=n->next;
+    Node* node1;
+    node1=n;
+        while(node1!=NULL){
+            //cout<<node1->data;
+            node1=node1->next;
             count++;
-            cout<<count<<endl;
+            //cout<<count<<endl;
         }
+        int i=0;
+        Node* next1;
+        while(i<count/2 -1)
+        {   
+            n=n->next;
+            i++;
+        }
+        if(count % 2 ==0){
+            next1=n->next;
+            cout<<((n->data+next1->data)/2);
+        }
+        else{
+            cout<<n->data;
+        }
+        
     }
  
 
@@ -32,14 +49,19 @@ int main(){
     Node* second=new Node();
     Node* third=new Node();
     Node* last=new Node();
+    Node* fourth=new Node();
 
     head->data=10;
     head->next=first;
     first->data=20;
     first->next=second;
     second->data=30;
-    second->next=last;
-    last->data=40;
+    second->next=third;
+    third->data=40;
+    third->next=fourth;
+    fourth->data=50;
+    fourth->next=last;
+    last->data=60;
     last->next=NULL;
     printNode(head);
     
---
+++ examples/mergeLists.cpp
@@ -0,0 +1,123 @@
+#include <iostream>
+#include <vector>
+#include <string.h>
+#include <bits/stdc++.h>
+#include <math.h>
+using namespace std;
+class ListNode{
+    public:
+    int val;
+    ListNode* next;
+};
+ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+    ListNode* n1=list1;
+    ListNode* n2=list2;
+    ListNode* checkNode=new ListNode();
+    ListNode* head;
+    if(list1==NULL){
+        return list2;
+    }
+    if(list2==NULL){
+        return list1;
+    }
+    
+    if(n1->val<=n2->val)
+        {   
+            checkNode=n1;        
+            n1=n1->next;
+        }
+        else
+         {  
+            checkNode=n2;
+            n2=n2->next;    
+        }
+
+    head=checkNode;
+    while(n1!=NULL && n2 !=NULL){
+        
+        if(n1->val<=n2->val)
+        {
+            checkNode->next=n1;
+            checkNode=checkNode->next; 
+            n1=n1->next;
+        }
+        else
+         {
+            checkNode->next=n2;
+            checkNode=checkNode->next; 
+            n2=n2->next;    
+        }
+
+        
+    }
+    if(n1==NULL){
+        checkNode->next=n2;
+        
+    }
+    else if (n2==NULL){
+        checkNode->next=n1;
+    }
+
+return head;
+
+    }
+void print(ListNode* head)
+    {   ListNode* temp=head;
+        
+        while (temp!=nullptr) 
+    {
+        cout << temp->val << " ";
+        temp = temp->next;
+    }
+    
+    }
+    int main(){
+    ListNode* list1=new ListNode();
+    ListNode* first=new ListNode();
+    ListNode* second=new ListNode();
+    ListNode* third=new ListNode();
+    ListNode* last=new ListNode();
+    ListNode* fourth=new ListNode();
+
+    list1->val=1;
+    list1->next=first;
+    first->val=3;
+    first->next=second;
+    second->val=4;
+    second->next=third;
+    third->val=4;
+    third->next=fourth;
+    fourth->val=5;
+    fourth->next=last;
+    last->val=6;
+    last->next=NULL;
+    ListNode* list2=new ListNode();
+    ListNode* fir=new ListNode();
+    ListNode* seco=new ListNode();
+    ListNode* thi=new ListNode();
+    ListNode* las=new ListNode();
+    ListNode* fourt=new ListNode();
+
+    list2->val=1;
+    list2->next=fir;
+    fir->val=2;
+    fir->next=seco;
+    seco->val=5;
+    seco->next=thi;
+    thi->val=5;
+    thi->next=fourt;
+    fourt->val=6;
+    fourt->next=las;
+    las->val=7;
+    las->next=NULL;
+    
+    ListNode *linkedList1=mergeTwoLists( list1, list2);
+    print(linkedList1);
+    // linkedList1=new ListNode(1,new ListNode(2, new ListNode(5)));
+    // ListNode *linkedList2;
+    // linkedList2=new ListNode(1,new ListNode(3,new ListNode(4)));
+
+    
+
+
+}
